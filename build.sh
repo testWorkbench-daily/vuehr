892d3a987fcbc6925267b26983a6549b60a07488
testWorkbench-daily <weaverhong@hotmail.com>
2021-12-01 17:59:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ npm install
 npm install @vue/cli-service
 
 npm install -g cnpm --registry=https://registry.npmmirror.com
-cnpm install node-sass --save
+cnpm install node-sass@4.0.0 --save
 
 npm run build 
